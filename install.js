3ec4c439167514928ab0c50a65e1b61c51ef4a3a
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-08-02 08:50:55 +0000

    Update title in install banner for clarity

--- install.js
+++ install.js
@@ -161,7 +161,7 @@
     banner.innerHTML = `
       <img src="icon-192.png" alt="icon" />
       <div class="ib-text">
-        <div class="ib-title">AlatiphA ICT for JHS</div>
+        <div class="ib-title">ICT for JHS (Computing)</div>
         <div class="ib-sub">Add to Home Screen for offline reading</div>
       </div>
       <div class="ib-actions">
@@ -225,7 +225,7 @@
       banner.id = "iosBanner";
       banner.innerHTML = `
         <div class="ios-title">
-          📖 Install  AlatiphA ICT for JHS
+          📖 Install  ICT for JHS (Computing)
         </div>
         <div class="ios-steps">
           Tap <strong>Share</strong> (□↑) at the bottom of Safari<br>
